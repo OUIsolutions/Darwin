a876bd5cf3a89d14c8cfa473f3ef05f6c779187e
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-10-24 17:43:38 -0300

    att

--- darwinconf.lua
+++ darwinconf.lua
@@ -1,7 +1,8 @@
 
-Addfile("test.lua")
+Addcode("print('hello world')") -- embed these code to o final.o
+Addfile("test.lua") --embed these file to final.o
 
-Cfilename = "final.c"
-Compiler = "gcc"
-Output = "final.o"
-Runafter = true
+Cfilename = "final.c" 
+Compiler = "gcc" 
+Output = "final.o"  
+Runafter = true 
