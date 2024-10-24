d3c73366df07ceb784d68dbebbff42a9ae2e0c7c
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-10-24 15:21:39 -0300

    att

--- build.sh
+++ build.sh
@@ -1,3 +1,3 @@
 
-curl -L https://github.com/OUIsolutions/Darwin/releases/download/0.001/darwin.c -o darwin.c
-gcc darwin.c -o darwin.o && ./darwin.o && mv  final.c darwin002.c
+curl -L https://github.com/OUIsolutions/Darwin/releases/download/0.002/darwin002.c -o darwin002.c
+gcc darwin002.c -o darwin002.o && ./darwin002.o && gcc final002.c -o final002.o
