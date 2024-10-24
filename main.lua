e8b2202261299bc531a596bdb175d0befa3a3942
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-10-24 17:00:52 -0300

    att

--- main.lua
+++ main.lua
@@ -31796,12 +31796,12 @@ mainfile:close()
 local size   = string.len(main_code)
 local buffer = { "unsigned char exec_code[] = {" }
 for i = 1, size do
-
-    local current_char = string.sub(main_code, i, i)
-    local byte = string.byte(current_char)
-    buffer[#buffer + 1] = string.format("%d,", byte)
+      local current_char = string.sub(main_code, i, i)
+      local byte = string.byte(current_char)
+      buffer[#buffer + 1] = string.format("%d,", byte)
+    
 end
 buffer[#buffer + 1] = "0,};\n"
-local formmated_main_code = buffer.join("")
+local formmated_main_code =  table.concat(buffer, "")
 local final = LUA_CEMBED .. formmated_main_code .. MAIN_C
 io.open("final002.c", "w"):write(final):close()
