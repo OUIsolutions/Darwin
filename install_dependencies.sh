ddf220b3967341fa682dc27089a11de89ff72c11
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-10-24 12:34:17 -0300

    at

--- install_dependencies.sh
+++
@@ -1,5 +0,0 @@
-mkdir dependencies
-curl -L https://github.com/OUIsolutions/CTextEngine/releases/download/v2.002/CTextEngine.h -o dependencies/CTextEngine.h
-curl -L https://github.com/OUIsolutions/DoTheWorld/releases/download/v7.004/doTheWorld.h -o dependencies/doTheWorld.h
-curl -L https://github.com/OUIsolutions/LuaCEmbed/releases/download/v0.76/LuaCEmbed.h -o dependencies/LuaCEmbed.h
-curl -L https://github.com/OUIsolutions/Universal-Garbage-Colector/releases/download/v2.003/UniversalGarbage.h -o dependencies/UniversalGarbage.h
