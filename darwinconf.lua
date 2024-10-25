c5d80c12fd4ea5038ddcaecadcc09ae58949e8ea
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-10-25 13:41:43 -0300

    att

--- darwinconf.lua
+++ darwinconf.lua
@@ -1,9 +1,9 @@
 
-Addfile("luacembed.lua")
-Addfile("main_c.lua")
-Addfile("globals.lua")
-Addfile("comp_time_functions.lua")
-Addfile("main.lua")
+Addfile("src/luacembed.lua")
+Addfile("src/main_c.lua")
+Addfile("src/globals.lua")
+Addfile("src/comp_time_functions.lua")
+Addfile("src/main.lua")
 
 Cfilename = "darwin004.c"
 Compiler = "gcc"
