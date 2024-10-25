d216cd0ba73f4cde967ab5f766bc46bddf1baf23
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-10-25 19:28:00 -0300

    att

--- src/embed/embed.lua
+++ src/embed/embed.lua
@@ -2,6 +2,9 @@
 ---@param var any
 ---@param mode "c" | "lua"
 function Embedglobal(name, var, mode)
+    if not mode then
+        mode = "c"
+    end
     if mode == "c" then
         Private_embed_global_in_c(name, var)
     end
