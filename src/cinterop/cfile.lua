a7215bc1b5c94cd7d8d517475a2e793880fe1115
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-10-31 07:50:54 -0300

    amalgamating runing

--- src/cinterop/cfile.lua
+++ src/cinterop/cfile.lua
@@ -12,35 +12,58 @@ function PrivateDarwin_Addcfile(contents_list, already_included, filename)
         return
     end
     already_included[#already_included + 1] = content
-
-    local COLLECTING_CHAR = 0
-    local WAITING_INCLUDE = 1
-    local COLLECTING_FILE = 2
-    local INSIDE_INLINE_COMMENT = 3
-    local INSIDE_BLOCK_COMMENT = 4
-    local state = COLLECTING_CHAR
-    local current_dir = PrivateDarwin_extract_dir(filename)
-    local actual_filename = current_dir
+    local START_STATE                       = 0
+    local WAITING_INCLUDE                   = 1
+    local COLLECTING_FILE                   = 2
+    local INSIDE_INLINE_COMMENT             = 3
+    local INSIDE_BLOCK_COMMENT              = 4
+    local INSIDE_CHAR                       = 5
+    local INSIDE_STRING                     = 6
+    local state                             = START_STATE
+    local current_dir                       = PrivateDarwin_extract_dir(filename)
+    local actual_filename                   = current_dir
+    local VALID_CHAR_BUFFER                 = {
+        INSIDE_STRING,
+        INSIDE_CHAR,
+        START_STATE,
+        INSIDE_INLINE_COMMENT,
+        INSIDE_BLOCK_COMMENT
+    }
 
     for i = 1, #content do
         local current_char = string.sub(content, i, i)
         local last_char = string.sub(content, i - 1, i - 1)
 
-        if state == COLLECTING_CHAR and PrivateDarwin_is_string_at_point(content, "//", i) then
+        if state == START_STATE and current_char == "'" then
+            state = INSIDE_CHAR
+        end
+        if state == INSIDE_CHAR and current_char == "'" then
+            state = START_STATE
+        end
+        if state == START_STATE and current_char == '"' then
+            state = INSIDE_STRING
+        end
+        if state == INSIDE_STRING and current_char == '"' and last_char ~= '"' then
+            state = START_STATE
+        end
+
+
+        if state == START_STATE and PrivateDarwin_is_string_at_point(content, "//", i) then
             state = INSIDE_INLINE_COMMENT
         end
 
         if state == INSIDE_INLINE_COMMENT and current_char == "\n" then
-            state = COLLECTING_CHAR
+            state = START_STATE
         end
 
 
-        if state == COLLECTING_CHAR and PrivateDarwin_is_string_at_point(content, "/*", i) then
+        if state == START_STATE and PrivateDarwin_is_string_at_point(content, "/*", i) then
             state = INSIDE_BLOCK_COMMENT
         end
 
+
         if state == INSIDE_BLOCK_COMMENT and PrivateDarwin_is_string_at_point(content, "*/", i) then
-            state = COLLECTING_CHAR
+            state = START_STATE
         end
 
 
@@ -49,7 +72,7 @@ function PrivateDarwin_Addcfile(contents_list, already_included, filename)
         end
 
         if state == WAITING_INCLUDE and last_char == "<" then
-            state = COLLECTING_CHAR
+            state = START_STATE
             --anulate the inclusion because its a local inclusion
             contents_list[#contents_list + 1] = "#include <"
         end
@@ -61,7 +84,7 @@ function PrivateDarwin_Addcfile(contents_list, already_included, filename)
             actual_filename = actual_filename .. current_char
         end
 
-        if PrivateDarwin_is_inside({ COLLECTING_CHAR, INSIDE_INLINE_COMMENT, INSIDE_BLOCK_COMMENT }, state) then
+        if PrivateDarwin_is_inside(VALID_CHAR_BUFFER, state) then
             contents_list[#contents_list + 1] = current_char
         end
 
@@ -69,7 +92,7 @@ function PrivateDarwin_Addcfile(contents_list, already_included, filename)
         if state == COLLECTING_FILE and current_char == '"' then
             PrivateDarwin_Addcfile(contents_list, already_included, actual_filename)
             actual_filename = current_dir
-            state = COLLECTING_CHAR
+            state = START_STATE
         end
     end
     contents_list[#contents_list + 1] = "\n"
