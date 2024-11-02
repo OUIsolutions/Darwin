69ac93945d5c6079612fad0c7cabc07f93125b0a
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-11-02 08:07:10 -0300

    creating actions

--- assets/executable_src/args_entrie.lua
+++ assets/executable_src/args_entrie.lua
@@ -10,3 +10,14 @@ function get_arg_number(arg_num)
     end
     return converted
 end
+
+function get_operator()
+    local operator        = arg[3]
+    local VALID_OPERATORS = { "+", "-", "*", "/" }
+    for i = 1, #VALID_OPERATORS do
+        if VALID_OPERATORS[i] == operator then
+            return operator
+        end
+    end
+    return nil
+end
