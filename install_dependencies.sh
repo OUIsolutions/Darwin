ed97b2bb6cb627c572491ca432aba4ac0cb9a1e4
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-11-08 03:16:59 -0300

    att

--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,4 +3,4 @@ rm -rf candangoEngine
 git clone -b v0.71 https://github.com/OUIsolutions/LuaDoTheWorld.git
 git clone -b V0.001 https://github.com/SamuelHenriqueDeMoraisVitrio/candangoEngine.git
 curl -L https://github.com/OUIsolutions/Darwin/releases/download/0.011/darwin011.c -o darwin011.c
-curl -L https://github.com/OUIsolutions/LuaCEmbed/releases/download/v0.77/LuaCEmbed.h -o assets/LuaCEmbed.h
+curl -L https://github.com/OUIsolutions/LuaCEmbed/releases/download/v0.778/LuaCEmbed.h -o assets/LuaCEmbed.h
