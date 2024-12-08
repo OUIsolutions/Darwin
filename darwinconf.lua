2a47c36f031eda14a10ca27ba6afee23c720df13
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-12-08 00:46:12 -0300

    att

--- darwinconf.lua
+++ darwinconf.lua
@@ -1,7 +1,13 @@
 -- eliminantes unwanted prints
-darwin.add_c_code("#undef LUA_USE_DLOPEN")
 
-darwin.add_c_file("LuaDoTheWorld/src/one.c", true)
+darwin.add_c_file("LuaDoTheWorld/src/one.c", true, function(import, path)
+    -- to make the luacembe not be imported twice
+    if import == "../dependencies/dependency.LuaCEmbed.h" then
+        return false
+    end
+    return true
+end)
+
 darwin.add_c_file("candangoEngine/src/main.c", true, function(import, path)
     -- to make the luacembe not be imported twice
     if import == "../dependencies/depB.LuaCEmbed.h" then
--- install_dependencies.sh
+++ install_dependencies.sh
@@ -3,5 +3,4 @@ rm -rf candangoEngine
 git clone -b v0.72 https://github.com/OUIsolutions/LuaDoTheWorld.git
 git clone -b V0.003 https://github.com/SamuelHenriqueDeMoraisVitrio/candangoEngine.git
 curl -L https://github.com/OUIsolutions/LuaCEmbed/releases/download/v0.779/LuaCEmbed.h -o assets/LuaCEmbed.h
-curl -L https://github.com/OUIsolutions/Darwin/releases/download/0.013/darwin013.c -o darwin.c
-gcc darwin.c -o darwin.o
+curl -L https://github.com/OUIsolutions/Darwin/releases/download/0.014/darwin.o -o darwin.0
