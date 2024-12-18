a248e58eff880a6310328c4204d37cf63c0d1d53
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-12-18 01:43:33 -0300

    att

--- types/private_darwin.lua
+++ types/private_darwin.lua
@@ -1,16 +1,4 @@
----@class Actions
----@field description string
----@field name string
----@field callback fun()
 
----@class PrivateDarwinModuleInternal
----@field item string
----@field content string
-
----@class PrivateDarwinSHaredLib
----@field sha_item string
----@field content string
----@field filename string
 
 ---@class PrivateDarwin
 ---@field is_string_at_point fun(str:string,target:string,point:number):boolean
