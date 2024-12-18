40bea29f2ab2cbf6dcada97f34283e9c84c4c205
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-12-18 01:51:53 -0300

    att

---
+++ types/cli/private_darwin.lua
@@ -0,0 +1,17 @@
+---@class PrivateDarwin
+---@field OPEN string
+---@field CLOSE string
+---@field PERCENT string
+---@field BREAK_LINE string
+---@field main fun()
+---@field print_color fun(color:string,text:string)
+---@field print_red fun(color:string,text:string)
+---@field print_green fun(color:string,text:string)
+---@field print_blue fun(color:string,text:string)
+---@field count_bars fun(str:string):number
+---@field get_asset fun(src:string):string | nil
+---@field list_assets_recursivly fun(src:string):string[]
+---@field list_assets fun(src:string):string[]
+
+---@type PrivateDarwin
+private_darwin = private_darwin
