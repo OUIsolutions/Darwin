3773da538a6ffdc3f72600ec1bbf5e8c0398eacd
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-12-18 01:39:21 -0300

    att

--- types/private_darwin.lua
+++ types/private_darwin.lua
@@ -14,6 +14,7 @@
 
 ---@class PrivateDarwin
 ---@field is_string_at_point fun(str:string,target:string,point:number):boolean
+---@field starts_with fun(str:string,target:string):boolean
 
 
 ---@type PrivateDarwin
