112b85bd6022bfb55aacc259fe60e51b16d90c8d
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-12-22 12:06:06 -0300

    att

--- types/api/private_darwin.lua
+++
@@ -1,7 +0,0 @@
----@class PrivateDarwin
----@field is_string_at_point fun(str:string,target:string,point:number):boolean
----@field starts_with fun(str:string,target:string):boolean
----@field is_inside fun(array:any[],value:any):boolean
-
----@type PrivateDarwin
-private_darwin = private_darwin
--- types/cli/consts.lua
+++
@@ -1,9 +0,0 @@
----@class Asset
----@field path string
----@field content string | nil
-
----@type Asset[]
-PRIVATE_DARWIN_ASSETS = PRIVATE_DARWIN_ASSETS
-
----@type string
-PRIVATE_DARWIN_TYPES = PRIVATE_DARWIN_TYPES
