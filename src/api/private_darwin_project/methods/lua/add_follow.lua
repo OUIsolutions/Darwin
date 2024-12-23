d4e75bdba6e479b6d94c2275c62ce7f163b2f787
mateusmoutinho <mateusmoutinho01@gmail.com>
2024-12-22 23:23:06 -0300

    starting working

--- src/api/private_darwin_project/methods/lua/add_follow.lua
+++ src/api/private_darwin_project/methods/lua/add_follow.lua
@@ -4,43 +4,92 @@ private_darwin_project.add_lua_file_followin_require_recursively = function(self
         error("content of " .. src .. "not provided")
     end
 
+    local SMALL_STRING              = 1
+    local NORMAL_STRING             = 2
+    local BIG_STRING                = 3
+
     local NORMAL_STATE              = 1
     local WAITING_REQUIRE_STRING    = 2
     local COLLECTING_REQUIRE_STRING = 3
     local state                     = NORMAL_STATE
-    local current_string_identifier = nil
+    local current_string_identifier = 0
     local require_buffer            = ""
-    for i = 1, #content do
-        local current_char = string.sub(content, i, i)
+    local i                         = 1
+
+    while true do
+        if i > #content then
+            break
+        end
+        --print("char:" .. string.sub(content, i, i) .. "state: " .. state .. " identifier" .. current_string_identifier)
+        if private_darwin.is_string_at_point(content, "\\", i) then
+            goto continue
+        end
+
         if state == NORMAL_STATE then
             if private_darwin.is_string_at_point(content, "require", i) then
                 state = WAITING_REQUIRE_STRING
+                i = i + #"require" - 1
                 goto continue
             end
         end
 
         if state == WAITING_REQUIRE_STRING then
-            if current_char == "(" then
+            if private_darwin.is_string_at_point(content, "(", i) then
                 goto continue
-            end
-            if current_char == '"' then
+            elseif private_darwin.is_string_at_point(content, " ", i) then
+                goto continue
+            elseif private_darwin.is_string_at_point(content, "'", i) then
                 state = COLLECTING_REQUIRE_STRING
-                current_string_identifier = current_char
+                current_string_identifier = SMALL_STRING
                 goto continue
+            elseif private_darwin.is_string_at_point(content, '"', i) then
+                state = COLLECTING_REQUIRE_STRING
+                current_string_identifier = NORMAL_STRING
+                goto continue
+            elseif private_darwin.is_string_at_point(content, "[[", i) then
+                state                     = COLLECTING_REQUIRE_STRING
+                current_string_identifier = BIG_STRING
+                i                         = i + #"[[" - 1
+
+                goto continue
+            else
+                error("invalid value at require index: " .. i .. " of file" .. src)
             end
         end
 
         if state == COLLECTING_REQUIRE_STRING then
-            if current_char == current_string_identifier then
+            local found_end = false
+            if current_string_identifier == SMALL_STRING then
+                if private_darwin.is_string_at_point(content, "'", i) then
+                    found_end = true
+                end
+            end
+            if current_string_identifier == NORMAL_STRING then
+                if private_darwin.is_string_at_point(content, '"', i) then
+                    found_end = true
+                end
+            end
+            if current_string_identifier == BIG_STRING then
+                if private_darwin.is_string_at_point(content, ']]', i) then
+                    found_end = true
+                end
+            end
+
+            if found_end then
                 print(require_buffer)
-                require_buffer = ""
                 state = NORMAL_STATE
+                require_buffer = ""
+                current_string_identifier = 0
                 goto continue
             end
-            require_buffer = require_buffer .. current_char
+
+            if not found_end then
+                require_buffer = require_buffer .. string.sub(content, i, i)
+            end
         end
 
         ::continue::
+        i = i + 1
     end
 end
 
