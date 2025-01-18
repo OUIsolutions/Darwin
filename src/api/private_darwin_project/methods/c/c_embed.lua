7435f32ca6fe7241a713cf1dc994841d98ffabc1
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-01-18 07:58:52 -0300

    att

--- src/api/private_darwin_project/methods/c/c_embed.lua
+++ src/api/private_darwin_project/methods/c/c_embed.lua
@@ -15,9 +15,11 @@ private_darwin_project.create_c_str_buffer = function(str_code, streamed_shas, s
 end
 
 
-private_darwin_project.embed_c_table = function(var,acumulator, streamed_shas,stream)
-    stream("LuaCEmbedTable *private_darwin_table%d = LuaCembed_new_anonymous_table(darwin_main_obj);\n")
+private_darwin_project.embed_c_table = function(var,total_tables, streamed_shas,stream)
     
+    local table_name = "private_darwin_table" .. total_tables
+    stream("LuaCEmbedTable *%s = LuaCembed_new_anonymous_table(darwin_main_obj);\n",table_name)
+    total_tables = total_tables + 1
     for key, val in pairs(current_table) do
         local key_type = type(key)
         local valtype = type(val)
@@ -39,47 +41,45 @@ private_darwin_project.embed_c_table = function(var,acumulator, streamed_shas,st
         end
 
         if key_type == "number" and valtype == "string" then
-            private_darwin.c_global_concat(string.format(
-                "LuaCEmbedTable_set_raw_string_by_index(%s,%d,(const char*)%s,%d);\n",
+            
+            local sha_name = private_darwin_project.create_c_str_buffer(val, streamed_shas, stream)
+            stream(string.format(
+                "LuaCEmbedTable_set_raw_string_by_index(%s,%d,%s,%d);\n",
                 table_name,
                 key - 1,
-                private_darwin.create_c_str_bufer(val),
+                sha_name,
                 string.len(val)
             ))
+            
+            
         end
 
         if key_type == "number" and val == true then
-            private_darwin.c_global_concat(string.format(
+            stream(string.format(
                 "LuaCEmbedTable_set_bool_by_index(%s,%d,true);\n",
                 table_name,
                 key - 1
             ))
         end
         if key_type == "number" and val == false then
-            private_darwin.c_global_concat(string.format(
+            stream(string.format(
                 "LuaCEmbedTable_set_bool_by_index(%s,%d,false);\n",
                 table_name,
                 key - 1
             ))
         end
         if key_type == "number" and valtype == "table" then
-            local sub_name = 'x' .. private_darwin.cglobals_size
-            private_darwin.cglobals_size = private_darwin.cglobals_size + 1
-            private_darwin.c_global_concat(string.format(
-                'LuaCEmbedTable *%s = LuaCembed_new_anonymous_table(main_obj);\n',
-                sub_name
-            ));
-            private_darwin.c_global_concat(string.format(
-                'LuaCEmbedTable_set_sub_table_by_index(%s,%d,%s);\n',
+            local created_name =  private_darwin_project.embed_c_table(val,total_tables,streamed_shas,stream)
+            stream(string.format(
+                "LuaCEmbedTable_set_table_by_index(%s,%d,%s);\n",
                 table_name,
                 key - 1,
-                sub_name
-            ));
-            private_darwin.embed_c_table(original_name, sub_name, val)
+                created_name
+            ))
         end
 
         if key_type == "string" and valtype == "number" then
-            private_darwin.c_global_concat(string.format(
+            stream(string.format(
                 "LuaCEmbedTable_set_double_prop(%s,%q,%f);\n",
                 table_name,
                 key,
@@ -89,23 +89,24 @@ private_darwin_project.embed_c_table = function(var,acumulator, streamed_shas,st
 
 
         if key_type == "string" and valtype == "string" then
-            private_darwin.c_global_concat(string.format(
-                "LuaCEmbedTable_set_raw_string_prop(%s,%q,(const char *)%s,%d);\n",
+            local sha_name = private_darwin_project.create_c_str_buffer(val, streamed_shas, stream)
+            stream(string.format(
+                "LuaCEmbedTable_set_raw_string_prop(%s,%q,%s,%d);\n",
                 table_name,
                 key,
-                private_darwin.create_c_str_bufer(val),
+                sha_name,
                 string.len(val)
             ))
         end
         if key_type == "string" and val == true then
-            private_darwin.c_global_concat(string.format(
+            stream(string.format(
                 "LuaCEmbedTable_set_bool_prop(%s,%q,true);\n",
                 table_name,
                 key
             ))
         end
         if key_type == "string" and val == false then
-            private_darwin.c_global_concat(string.format(
+            stream(string.format(
                 "LuaCEmbedTable_set_bool_prop(%s,%q,false);\n",
                 table_name,
                 key
@@ -113,22 +114,16 @@ private_darwin_project.embed_c_table = function(var,acumulator, streamed_shas,st
         end
 
         if key_type == "string" and valtype == "table" then
-            local sub_name = 'x' .. private_darwin.cglobals_size
-            private_darwin.cglobals_size = private_darwin.cglobals_size + 1
-            private_darwin.c_global_concat(string.format(
-                'LuaCEmbedTable *%s = LuaCembed_new_anonymous_table(main_obj);\n',
-                sub_name
-            ));
-
-            private_darwin.c_global_concat(string.format(
-                'LuaCEmbedTable_set_sub_table_prop(%s,%q,%s);\n',
+          local created_name =   private_darwin_project.embed_c_table(val,total_tables,streamed_shas,stream)
+            stream(string.format(
+                "LuaCEmbedTable_set_table_prop(%s,%q,%s);\n",
                 table_name,
                 key,
-                sub_name
-            ));
-            private_darwin.embed_c_table(original_name, sub_name, val)
+                created_name
+            ))
         end
     end
+    return table_name
 end
 
 
@@ -156,6 +151,6 @@ private_darwin_project.embed_global_in_c = function(name, var, var_type,streamed
         
     end
     if var_type == "table" then        
-        private_darwin_project.embed_c_table(var,streamed_shas,stream)
+        private_darwin_project.embed_c_table(var,0,streamed_shas,stream)
     end
 end
