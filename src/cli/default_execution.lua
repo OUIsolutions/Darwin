c8283575e259e9712ea5f6f4d3ace52fd217a6cd
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-01-23 03:36:42 -0300

    at

--- src/cli/default_execution.lua
+++ src/cli/default_execution.lua
@@ -1,29 +1,34 @@
 
+local valid_out_modes = {
+    "lua"= {"lua"},
+    "c"= {"c"},
+    "linux_bin"={"o", "out"},
+    "windows_bin"={"exe"},
+    "linux_so"={"so"},
+    "windows_dll"={"dll"}
+}
+
 local function get_default_output_mode(output)
     local extension = darwin.dtw.newPath(output).get_extension()
-    
-    if extension == "lua" then
-        return "lua"
+    for key,val in pairs(valid_out_modes) do
+        for i=1,#val do
+            if val[i] == extension then
+                return key
+            end
+        end
     end
-
-    if extension == "c" then
-        return "c"
-    end
-    if extension == "o" or extension == "out"then
-        return "linux_bin"
-    end 
-    if extension == "exe" then
-        return "windows_bin"
-    end
-    if extension == "so" then
-        return "linux_so"
-    end
-    if extension == "dll" then
-        return "windows_dll"
-    end
-
+    return nil
 
 end 
+
+function output_mode_its_valid(output_mode)
+    for key,_ in pairs(valid_out_modes) do
+        if key == output_mode then
+            return true
+        end
+    end
+    return false
+end
 
 function Default_execution()
     local output = darwin.argv.get_flag_arg_by_index({"o", "output"}, 1)
@@ -32,6 +37,16 @@ function Default_execution()
          1, 
          get_default_output_mode(output)
     )
+    if not output_mode then
+        private_darwin.print_red("output impossible to determine")
+        return
+    end
+    if not output_mode_its_valid(output_mode) then
+        private_darwin.print_red("output mode not valid")
+        return
+    end
+    
+
     local main_file = darwin.argv.get_next_unused()
     if not main_file then
         private_darwin.print_red("file not passed")
