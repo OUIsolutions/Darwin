95dda42b36a1d051bc6f78a7abbf559d34e78e4a
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-01-23 14:00:49 -0300

    att

--- src/cli/projects/dynamic_link_lib.lua
+++ src/cli/projects/dynamic_link_lib.lua
@@ -8,13 +8,6 @@ function create_dynamic_link_lib_project( project,output, output_mode)
         return 
     end
 
-    local cache_output = "darwin_cache.c"
-    darwin.dtw.remove_any(cache_output)
-    project.generate_c_lib_file({
-        output = cache_output,
-        object_export = object_export
-    })
-
     local compiler = nil
     if output_mode == "windows_dll" then
         compiler = darwin.argv.get_flag_arg_by_index({"compiler"}, 1,"i686-w64-mingw32-gcc")
@@ -23,6 +16,15 @@ function create_dynamic_link_lib_project( project,output, output_mode)
         compiler = darwin.argv.get_flag_arg_by_index({"compiler"}, 1,"gcc")
     end
     local flags = darwin.argv.get_flag_arg_by_index({"compiler_flags"}, 1,"")
+
+
+    local cache_output = "darwin_cache.c"
+    darwin.dtw.remove_any(cache_output)
+    project.generate_c_lib_file({
+        output = cache_output,
+        object_export = object_export
+    })
+
 
     os.execute(compiler.." -shared -fPIC "..cache_output.." -o "..output.." "..flags)
     
--- src/cli/projects/executabe_bin_project.lua
+++ src/cli/projects/executabe_bin_project.lua
@@ -1,26 +1,37 @@
 
 function create_executable_bin_project(project, output,output_mode)
 
+    local compiler = nil
+    if output_mode == "windows_bin" then
+        compiler = darwin.argv.get_first_flag_arg({"compiler"},"i686-w64-mingw32-gcc")
+    end 
+    if output_mode == "linux_bin" then
+        compiler = darwin.argv.get_first_flag_arg({"compiler"}, "gcc")
+    end
+
+    local flags_size = darwin.argv.get_first_flag_arg("flags")
+    local flags_text = ""
+    for i = 1, flags_size do
+        local flag = darwin.argv.get_compact_flags("flags:", i, "")
+        flags_text = flags_text .. " " .. flag
+    end
+
+    if not handle_main_file() then
+        return
+    end
+    
+    
+    if not handle_unused() then
+        return
+    end
+    
+
     local cache_output = "darwin_cache.c"
     darwin.dtw.remove_any(cache_output)
     project.generate_c_file({
         output = cache_output,
         include_embed_data = true
     })
-    local compiler = nil
-    if output_mode == "windows_bin" then
-        compiler = darwin.argv.get_flag_arg_by_index({"compiler"}, 1,"i686-w64-mingw32-gcc")
-    end 
-    if output_mode == "linux_bin" then
-        compiler = darwin.argv.get_flag_arg_by_index({"compiler"}, 1,"gcc")
-    end
-
-    local flags_size = darwin.argv.get_compact_flags_size("flags:")
-    local flags_text = ""
-    for i = 1, flags_size do
-        local flag = darwin.argv.get_compact_flags("flags:", i, "")
-        flags_text = flags_text .. " " .. flag
-    end
 
     os.execute(compiler.." "..cache_output.." -o "..output.." "..flags_text)
     
