043d4889b318f9dd35b5250ac78fb4bf03b8d0ed
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-01-23 06:48:35 -0300

    att

--- src/cli/embed_data.lua
+++ src/cli/embed_data.lua
@@ -5,12 +5,12 @@ function get_embed_vars(project)
     local embed_vars_size = darwin.argv.get_flag_size("embed_vars")
     for i=1,embed_vars_size do
         local embed_var = darwin.argv.get_flag_arg_by_index("embed_vars", i)
-        local possible_text = darwin.argv.get_compact_flags("text"..embed_var.."=",1)
-        local possible_file = darwin.argv.get_compact_flags("file"..embed_var.."=",1)
-        local possible_folder = darwin.argv.get_compact_flags("folder"..embed_var.."=",1)
+        local possible_text = darwin.argv.get_flag_arg_by_index(embed_var..":text",1)
+        local possible_file = darwin.argv.get_flag_arg_by_index(embed_var..":file",1)
+        local possible_folder = darwin.argv.get_flag_arg_by_index(embed_var..":folder",1)
         if not possible_text and not possible_file and not possible_folder then
-            private_darwin.print_red("embed_var not found")
-            return
+            private_darwin.print_red("embed_var not found\n")
+            return false
         end
         
         if possible_text then
@@ -18,7 +18,7 @@ function get_embed_vars(project)
         end
 
         if possible_file then
-            project.add_embedded_file(embed_var,darwin.file_stream(possible_file))
+            project.embed_global(embed_var,darwin.file_stream(possible_file))
         end
 
         if possible_folder then
@@ -40,5 +40,5 @@ function get_embed_vars(project)
         end
 
     end
-
+    return true
 end 
