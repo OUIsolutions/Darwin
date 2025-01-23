9fc6e1ee11d715c65239765eff4730cf2f369fc7
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-01-23 05:59:09 -0300

    att

---
+++ src/cli/drop.lua
@@ -0,0 +1,19 @@
+function Drop_types()
+    local file = darwin.argv.get_next_unused()
+    
+    if not file then
+        file = "darwintypes.lua"
+    end
+    darwin.dtw.write_file(file, PRIVATE_DARWIN_TYPES)
+end
+
+
+function Drop_lua_cembed()
+    local file = darwin.argv.get_next_unused()
+    
+    if not file then
+        file = "LuaCEmbed.lua"
+    end
+    local lua_cembedd = private_darwin.get_asset(PRIVATE_DARWIN_API_ASSETS, "LuaCEmbed.h")
+    darwin.dtw.write_file(file, lua_cembedd)
+end
--- src/cli/drop_types.lua
+++
@@ -1,8 +0,0 @@
-function Drop_types()
-    local file = darwin.argv.get_next_unused()
-    
-    if not file then
-        file = "darwintypes.lua"
-    end
-    darwin.dtw.write_file(file, PRIVATE_DARWIN_TYPES)
-end
