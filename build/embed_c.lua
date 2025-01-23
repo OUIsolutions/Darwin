a3785eb28fc2dcf013b16f2c4c310b02095c19dd
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-01-23 17:31:44 -0300

    att

--- build/embed_c.lua
+++ build/embed_c.lua
@@ -7,7 +7,6 @@ function Embed_c_code(project)
 
     project.add_c_file("dependencies/LuaDoTheWorld/src/one.c", true, function(path,import )
         -- to make the luacembe not be imported twice
-        print(import)
         if import == "../dependencies/dependency.LuaCEmbed.h" then
             return false
         end
