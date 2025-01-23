bdd8fdd590a679a2ed82660a2f2fae198b18d4c4
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-01-23 05:02:20 -0300

    att

--- src/cli/projects/executable_c_project.lua
+++ src/cli/projects/executable_c_project.lua
@@ -7,10 +7,9 @@ function create_executable_c_project(entry, output, output_mode)
     local relative_path = darwin.argv.get_flag_arg_by_index({"relative_path"}, 1)
     project.add_lua_file_following_require(entry,relative_path)
 
-    local cache_output = "darwin_cache.c"
     project.generate_c_file({
-        output = cache_output,
+        output = output,
         include_embed_data = true
     })
-    
+
 end
--- src/cli/projects/lib_source.lua
+++ src/cli/projects/lib_source.lua
@@ -1,15 +1,20 @@
 
 function create_lib_source_project(entry, output, output_mode)
 
-
     local default_name = dtw.path.newPath(output).get_only_name()
     local project_name = darwin.argv.get_flag_arg_by_index({"name"}, 1, default_name)
     local project = darwin.create_project(project_name)
     local relative_path = darwin.argv.get_flag_arg_by_index({"relative_path"}, 1)
-    project.add_lib_source_file(entry, relative_path)
+    project.add_lua_file_following_require(entry,relative_path)
+    local object_export = darwin.argv.get_flag_arg_by_index({"object_export"}, 1)
+    if not object_export then
+        private_darwin.print_red("Error: object_export is required")
+    end
 
-    project.generate_lib_source_file({
+    project.generate_c_lib_file({
+        lib_name=project_name,
         output = output,
-        include_embed_data = true
+        object_export = object_export
     })
+    
 end
