6212e08e012e04cdd06bf74ecdae3bfc8ffd4352
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-01-23 16:16:18 -0300

    att

--- build/assets.lua
+++ build/assets.lua
@@ -12,15 +12,18 @@ function Create_api_assets(project)
         }
     end
 
-    local assets_dirs = darwin.dtw.list_dirs_recursively("assets", false)
-    for i = 1, #assets_dirs do
-        local current_item          = assets_dirs[i]
-        ---removing / at the end of the the dir
-        current_item                = string.sub(current_item, 0, #current_item - 1)
-        api_assets[#api_assets + 1] = {
-            path = current_item
+    project.embed_global("PRIVATE_DARWIN_API_ASSETS", api_assets)
+
+    local cli_assets = {}
+    local cli_files = darwin.dtw.list_files_recursively("assets/cli", false)
+    for i = 1, #cli_files do
+        local current_item = cli_files[i]
+        local path = "assets/cli/" .. current_item
+        cli_assets[#cli_assets + 1] = {
+            path = current_item,
+            content = darwin.dtw.load_file(path)
         }
     end
 
-    project.embed_global("PRIVATE_DARWIN_API_ASSETS", api_assets)
+    project.embed_global("PRIVATE_DARWIN_CLI_ASSETS", cli_assets)
 end
