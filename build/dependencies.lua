352cd52011effbe7b6c95229b7ecd26773968d49
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-08-18 22:37:19 -0300

    upgraded dep

--- build/dependencies.lua
+++ build/dependencies.lua
@@ -12,7 +12,7 @@ function Install_all_dependencies()
         "mkdir dependencies",
         "cd dependencies && curl -L https://github.com/OUIsolutions/MDeclare/releases/download/0.1.0/MDeclareApiNoDependenciesIncluded.h -o MDeclareApiNoDependenciesIncluded.h",
         "cd dependencies && curl -L https://github.com/OUIsolutions/LuaFluidJson/releases/download/0.6.1/luaFluidJson_no_dep.c -o luaFluidJson_no_dep.c",
-        "cd dependencies && curl -L https://github.com/OUIsolutions/LuaDoTheWorld/releases/download/0.9.1/luaDoTheWorld_no_dep.c -o luaDoTheWorld_no_dep.c",
+        "cd dependencies && curl -L https://github.com/OUIsolutions/LuaDoTheWorld/releases/download/0.12.0/luaDoTheWorld_no_dep.c -o luaDoTheWorld_no_dep.c",
         "cd dependencies && git clone -b V0.003 https://github.com/SamuelHenriqueDeMoraisVitrio/candangoEngine.git",
         "cd dependencies && curl -L https://github.com/OUIsolutions/CTextEngine/releases/download/3.0.000/CTextEngineOne.c -o CTextEngineOne.c",
         "cd dependencies && curl -L https://github.com/OUIsolutions/LuaMDeclare/releases/download/0.1.0/luamdeclare.c -o luamdeclare.c",
@@ -20,7 +20,7 @@ function Install_all_dependencies()
         "cd dependencies && curl -L https://github.com/OUIsolutions/DoTheWorld/releases/download/11.0.0/doTheWorldOne.c -o doTheWorldOne.c",
         "cd dependencies && curl -L https://github.com/OUIsolutions/LuaArgv/releases/download/0.1.0/luargv.lua -o luargv.lua",
         'cd dependencies && curl -L https://github.com/OUIsolutions/LuaShip/releases/download/0.2.0/LuaShip.lua -o LuaShip.lua',
-        "cd dependencies && curl -L https://github.com/OUIsolutions/LuaSilverChain/releases/download/0.2.0/silverchain_no_dependecie_included.c -o silverchain_no_dependecie_included.c",
+        "cd dependencies && curl -L https://github.com/OUIsolutions/LuaSilverChain/releases/download/0.2.1/silverchain_no_dependecie_included.c -o silverchain_no_dependecie_included.c",
         "cd dependencies && curl -L https://github.com/OUIsolutions/LuaCAmalgamator/releases/download/0.1.0/lua_c_amalgamator_dependencie_not_included.c -o lua_c_amalgamator_dependencie_not_included.c"
     }
 
