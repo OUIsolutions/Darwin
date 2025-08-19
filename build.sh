5b45fa45c64c7425ffee9587624c5763712c4b39
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-08-19 15:09:25 -0300

    att

--- build.sh
+++ build.sh
@@ -3,14 +3,8 @@
 # Nome da imagem
 IMAGE_NAME="darwin_image"
 DOCKERFILE_PATH="dockerfile"
+docker build -f "$DOCKERFILE_PATH" -t "$IMAGE_NAME" .
 
-# Verifica se a imagem já existe
-if ! docker image inspect $IMAGE_NAME > /dev/null 2>&1; then
-    echo "Construindo a imagem Docker..."
-    docker build -f "$DOCKERFILE_PATH" -t "$IMAGE_NAME" .
-else
-    echo "Imagem Docker já existe."
-fi
 
 echo "Executando o container..."
 docker run --rm -v "$(pwd):/app" -w /app $IMAGE_NAME
--- dockerfile
+++ dockerfile
@@ -1,4 +1,5 @@
-FROM alpine:latest
-RUN apk add curl 
+FROM ubuntu:latest
+RUN apt update
+RUN apt install -y curl 
 RUN curl -L https://github.com/OUIsolutions/Darwin/releases/download/0.9.0/darwin.out -o darwin.out && chmod +x darwin.out && mv darwin.out /usr/bin/darwin
-CMD ["darwin run_blueprint build/ -mode folder amalgamation_build"]
+CMD ["/usr/bin/darwin", "run_blueprint", "build/", "-mode", "folder", "amalgamation_build"]
