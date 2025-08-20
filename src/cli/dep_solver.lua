757e63207b5666f7649daff255e5c1c92a2ef3d9
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-08-20 12:12:18 -0300

    att

--- src/cli/dep_solver.lua
+++ src/cli/dep_solver.lua
@@ -1,2 +1,14 @@
 
 
+function dep_solver()
+    local darwin_deps_json = darwin.dtw.load_file("darwindeps.json")
+    if not darwin_deps_json then
+        return 
+    end
+    local deps = darwin.json.load_from_string(darwin_deps_json)
+    for i=1,#deps do 
+        local current = deps[i]
+        print(current.type)
+    end 
+
+end 
