2d44564edd67564a386d75a1e6037563f9b44c55
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-08-20 19:00:34 -0300

    at

--- src/cli/dep_solver.lua
+++ src/cli/dep_solver.lua
@@ -1,7 +1,12 @@
 
-local function download_latest_with_curl(dep)
-    
-end 
+local function get_latest_version_from_repo(dep)
+    --https://api.github.com/repos/OUIsolutions/BearHttpsClient/releases/latest
+    local command = "curl -L " .. "https://api.github.com/repos/" .. dep.repo .. "/releases/latest -o temp"
+    os.execute(command)
+    local api_content = darwin.dtw.load_file("temp")
+    local json_content = darwin.json.load_from_string(api_content)
+    return json_content.name
+end
 
 local function release_download(dep)
 
@@ -19,13 +24,16 @@ local function release_download(dep)
     end
 
     if dep.cli == "curl" then
-        if dep.version then
-            local command = "curl -L https://github.com/" .. dep.repo .."/releases/download/"..dep.version.."/"..dep.file.." -o temp"
-            os.execute(command)
-        end
+        
+        local version = dep.version
         if not dep.version then
-            download_latest_with_curl(dep)
-        end 
+            version = get_latest_version_from_repo(dep)
+        end
+
+        local command = "curl -L https://github.com/" .. dep.repo .."/releases/download/"..version.."/"..dep.file.." -o temp"
+        os.execute(command)
+    
+    
     elseif dep.cli == "gh" then
         local version = dep.version or "latest"
         local command = "gh release download " .. version .. " -R " .. dep.repo .. " --pattern " .. dep.file .. " -D temp"
