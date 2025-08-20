2b402d4244c3278e535b409ffaae729537c2f262
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-08-20 12:16:58 -0300

    attt

--- src/cli/dep_solver.lua
+++ src/cli/dep_solver.lua
@@ -1,4 +1,7 @@
-
+function release_download(dep)
+end 
+function git_download(dep)
+end 
 
 function dep_solver()
     local darwin_deps_json = darwin.dtw.load_file("darwindeps.json")
@@ -8,7 +11,13 @@ function dep_solver()
     local deps = darwin.json.load_from_string(darwin_deps_json)
     for i=1,#deps do 
         local current = deps[i]
-        print(current.type)
+        if current.type == "release" then 
+            release_download(current) 
+        elseif current.type == "git" then
+            git_download(current)
+        else 
+            error("invalid dep type:"..current.type,0)
+        end 
     end 
 
 end 
