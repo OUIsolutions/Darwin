b8e7fd3cccf249455aba030526060bd7c0743ff9
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-08-21 01:51:02 -0300

    adapted new candango

--- builds/amalgamation_build.lua
+++ builds/amalgamation_build.lua
@@ -40,21 +40,13 @@ function Embed_c_code(project)
     project.add_c_file("dependencies/luaFluidJson_no_dep.c")
     project.add_c_file("dependencies/MDeclareApiNoDependenciesIncluded.h")
     project.add_c_file("dependencies/luamdeclare.c")
-
-    project.add_c_file("dependencies/candangoEngine/src/main.c", true, function(path,import)
-        -- to make the luacembe not be imported twice
-        if import == "../dependencies/depB.LuaCEmbed.h" then
-            return false
-        end
-        return true
-    end)
-   
+    project.add_c_file("dependencies/candangoEngine.c")
     project.load_lib_from_c("luaopen_luamdeclare", "private_darwin_luamdeclare")
     project.load_lib_from_c("luaopen_lua_c_amalgamator", "private_darwin_camalgamator")
     project.load_lib_from_c("luaopen_lua_silverchain", "private_darwin_silverchain")
     project.load_lib_from_c("load_luaDoTheWorld", "private_darwin_dtw")
     project.load_lib_from_c("load_lua_fluid_json", "private_darwin_json")
-    project.load_lib_from_c("candango_engine_start_point", "private_darwin_candango")
+    project.load_lib_from_c("luaopen_CandangoEngine_start_point", "private_darwin_candango")
 end
 
 
