ef68461634a50633c7fbe19e988ebe17625861f6
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-10-05 10:10:17 -0300

    att

--- devops.releasefier.lua
+++ devops.releasefier.lua
@@ -47,7 +47,7 @@ function main()
   cachify.execute_config({
       sources = {"src","dependencies","assets","darwinconf.lua","builds"},
       callback = function()
-           print("executed ")
+           print("executed callback for release")
            dtw.remove_any("release")
            shipyard.increment_replacer("release.json","PATCH_VERSION")
            os.execute("git add .")
