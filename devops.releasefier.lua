885857e3fb29fa5f6ea531173bfcc02277d47fa0
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-10-05 08:21:40 -0300

    att

--- devops.releasefier.lua
+++ devops.releasefier.lua
@@ -2,10 +2,12 @@ local cachify = load_global_module("cachify")
 
 os.execute("git restore .")
 os.execute("git pull")
+os.execute("darwin update darwindepos.json")
+
 
 cachify.execute_config({
     sources = {"darwindeps.json"},
-    callback = function() os.execute("darwin update darwindepos.json") end,
+    callback = function()   end,
     cache_name="teste",
     cache_dir=".cachify",
     ignore_first = true
