5e29a030b7bedc205a616bd1da886ffe9d7237ec
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-10-05 08:19:03 -0300

    att

---
+++ devops.releasefier.lua
@@ -0,0 +1,12 @@
+
+
+
+
+if true then return end 
+cachify.execute_config({
+    sources = {"darwindeps.json"},
+    callback = function() print("Callback executed!") end,
+    cache_name="teste",
+    cache_dir=".cachify",
+    ignore_first = true
+})
