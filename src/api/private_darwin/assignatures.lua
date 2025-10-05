dd3850a241cc37cd22de013ccd07f6c61e883b6a
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-10-05 04:13:44 -0300

    att

---
+++ src/api/private_darwin/assignatures.lua
@@ -0,0 +1,25 @@
+
+private_darwin.assignatures = {}
+
+private_darwin.get_item_assignature = function(item)
+    if darwin.dtw.isfile(item) then 
+        return darwin.dtw.generate_sha_from_file(item)
+    end
+    if darwin.dtw.isdir(item) then
+        return darwin.dtw.generate_sha_from_folder_by_last_modification(item)
+    end
+    return ""
+end 
+
+private_darwin.generate_assignature = function (elements)
+    
+    local hasher = darwin.dtw.newHash()
+    for i = 1, #elements do
+        local current_item = elements[i]
+        if not  private_darwin.assignatures[current_item] then 
+            private_darwin.assignatures[current_item] = private_darwin.get_item_assignature(current_item)
+        end 
+        hasher.digest(private_darwin.assignatures[current_item])
+
+    end
+end
