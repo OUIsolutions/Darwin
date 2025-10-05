a65adfcfe2543284a82a9fda7e13158861a080ce
mateusmoutinho <mateusmoutinho01@gmail.com>
2025-10-05 04:43:25 -0300

    att

--- src/api/darwin/create_project.lua
+++ src/api/darwin/create_project.lua
@@ -9,6 +9,7 @@ darwin.create_project = function(project_name)
         private_darwin_project.embed_global(selfobj, name, value)
     end
 
+    
     private_darwin_project.construct_globals(selfobj)
     private_darwin_project.add_lua_methods(selfobj)
     private_darwin_project.add_c_methods(selfobj)
